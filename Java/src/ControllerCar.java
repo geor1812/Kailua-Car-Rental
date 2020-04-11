6db94f1d4c0a5135e2b1190b9708be309fbadefb
George <george.ursachi7@gmail.com>
2020-04-11 14:25:51 +0200

    Added updateDB() and queryID() Started working on creating new cars

--- Java/src/ControllerCar.java
+++ Java/src/ControllerCar.java
@@ -2,7 +2,67 @@ import java.sql.*;
 
 public class ControllerCar {
     public static void main(String[] args) {
-        read();
+        create();
+    }
+
+    public static void create() {
+        System.out.println("[1] New Model\n[2] Existing Model");
+        int modelId = -1;
+        switch (Validation.menuSelection(1,2)) {
+            case 1:
+                modelId = createModel();
+                break;
+            case 2:
+                break;
+        }
+        System.out.println("Model ID = " + modelId);
+    }
+
+    public static int createModel() {
+        //Getting the data from the user
+        System.out.println("Enter the model group:\n" +
+                "[1] Luxury Car\n[2] Family Car\n[3] Sports Car");
+        int modelGroup = Validation.menuSelection(1,3);
+
+        System.out.println("Enter the brand");
+        String brand = Validation.getString();
+
+        System.out.println("Enter the model details");
+        String modelDetails = Validation.getString();
+
+        System.out.println("Enter the fuel type\n" +
+                "[1] E85 Gasoline\n[2] 90+ Octane Gasoline\n[3] Diesel");
+        String fuelType = "";
+        switch (Validation.menuSelection(1,3)) {
+            case 1:
+                fuelType = "E85 Gasoline";
+                break;
+            case 2:
+                fuelType = "90+ Octane Gasoline";
+                break;
+            case 3:
+                fuelType = "Diesel";
+                break;
+        }
+
+        //Inserting the model in the database
+        try {
+            String insertQuery = "INSERT INTO model\n" +
+                    "(model_group, brand, model_details, fuel_type)\n" +
+                    "VALUES\n" +
+                    "(" + modelGroup +",\'" + brand + "\',\'" + modelDetails + "\', \'" + fuelType + "\');";
+            DBConnection.updateDB(insertQuery);
+            //Shows the last item added in the table
+            String selectQuery = "SELECT * FROM model ORDER BY model_id DESC LIMIT 1";
+            DBConnection.queryDB(selectQuery);
+            System.out.println("Model succesfully added");
+
+            //Returns the id of the model which is needed in creating the car
+            return DBConnection.queryID("SELECT * FROM model ORDER BY model_id DESC LIMIT 1");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return -1;
     }
 
     public static void read() {
@@ -33,7 +93,7 @@ public class ControllerCar {
     //Shows cars after a search by attribute
     public static void readByAttribute() {
         System.out.println("By which attribute do you want to search?");
-        System.out.println("[1]Car ID\n[2] Model ID\n[3] Model Group\n[4] Brand\n[5] Registration Number");
+        System.out.println("[1] Car ID\n[2] Model ID\n[3] Model Group\n[4] Brand\n[5] Registration Number");
         try {
             switch (Validation.menuSelection(1,5)) {
                 case 1:
--- Java/src/DBConnection.java
+++ Java/src/DBConnection.java
@@ -49,4 +49,73 @@ public class DBConnection {
             System.exit(1);
         }
     }
+
+    //Returns the first entry in the table usually an ID
+    //Very useful when adding objects that are stored in multiple tables in the DB
+    public static int queryID(String query) throws SQLException {
+        try {
+            con = null;
+            Statement s = null;
+            Class.forName (JDBC_DRIVER);
+
+            con = DriverManager.getConnection(DATABASE_URL, "root", "password"); //change PW to match yours
+            s = con.createStatement();
+
+            ResultSet rs = s.executeQuery(query);
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int id = -1;
+            if (rs != null) {
+                while(rs.next()) {
+                    id = rs.getInt(1);
+                }
+            }
+            s.close();
+            con.close();
+            return id;
+        }
+        catch (SQLException sqlex) {
+            try{
+                System.out.println(sqlex.getMessage());
+                con.close();
+                System.exit(1);
+            }
+            catch(SQLException sql) {}
+        }
+        catch (ClassNotFoundException noClass) {
+            System.err.println("Driver Class not found");
+            System.out.println(noClass.getMessage());
+            System.exit(1);
+        }
+        return -1;
+    }
+
+    //Executes a data manipulation query
+    public static void updateDB(String query) throws SQLException {
+        try {
+            con = null;
+            Statement s = null;
+            Class.forName (JDBC_DRIVER);
+
+            con = DriverManager.getConnection(DATABASE_URL, "root", "password"); //change PW to match yours
+            s = con.createStatement();
+
+            int rs = s.executeUpdate(query);
+            s.close();
+            con.close();
+        }
+        catch (SQLException sqlex) {
+            try{
+                System.out.println(sqlex.getMessage());
+                con.close();
+                System.exit(1);
+            }
+            catch(SQLException sql) {}
+        }
+        catch (ClassNotFoundException noClass) {
+            System.err.println("Driver Class not found");
+            System.out.println(noClass.getMessage());
+            System.exit(1);
+        }
+    }
+
 }
